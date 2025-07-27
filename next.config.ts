894014bd6fbf548c90fb7bb323661b72ff95c233
Shovit Dutta <shovitdutta1@gmail.com>
2025-07-27 11:52:27 +0530

    kontext

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,8 @@
-import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-    images: {
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        domains: ["via.placeholder.com", "source.unsplash.com"],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        formats: ["image/webp"],
-    },
-    experimental: {
-        optimizeCss: true,
-        workerThreads: true,
-        optimisticClientCache: true,
-    },
-    compress: true,
+    images: { deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], remotePatterns: [{ protocol: "https", hostname: "**" }], imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], formats: ["image/webp"] },
     reactStrictMode: true,
-    poweredByHeader: false,
+    compress: true,
 };
 export default nextConfig;
